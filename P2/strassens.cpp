e04a2e82878544cdb3f7543d7b9ce5bea378b249
Dylan Bruckner <dylanbruckner@college.harvard.edu>
2025-03-30 16:36:56 -0400

    minor bugfixes

--- P2/strassens.cpp
+++ P2/strassens.cpp
@@ -104,56 +104,57 @@ void strassen_mult_recursion(const Matrix<int>& M1, const Matrix<int>& M2, Matri
     return;
 }
 
-Matrix<int> strassen_mult(const Matrix<int>& M1, const Matrix<int>& M2) {
-    const size_t n = M1.getDim();
+// old Strassen
+// Matrix<int> strassen_mult(const Matrix<int>& M1, const Matrix<int>& M2) {
+//     const size_t n = M1.getDim();
     
-    // base case: switch to standard multiplication
-    if(n <= CUTOFF) {
-        return matrix_mult(M1, M2);
-    }
+//     // base case: switch to standard multiplication
+//     if(n <= CUTOFF) {
+//         return matrix_mult(M1, M2);
+//     }
 
-    // Split matrices into quarters
-    const size_t half = n / 2;
-    auto A = M1.submatrix(0, 0, half);
-    auto B = M1.submatrix(0, half, half);
-    auto C = M1.submatrix(half, 0, half);
-    auto D = M1.submatrix(half, half, half);
+//     // Split matrices into quarters
+//     const size_t half = n / 2;
+//     auto A = M1.submatrix(0, 0, half);
+//     auto B = M1.submatrix(0, half, half);
+//     auto C = M1.submatrix(half, 0, half);
+//     auto D = M1.submatrix(half, half, half);
 
-    auto E = M2.submatrix(0, 0, half);
-    auto F = M2.submatrix(0, half, half);
-    auto G = M2.submatrix(half, 0, half);
-    auto H = M2.submatrix(half, half, half);
+//     auto E = M2.submatrix(0, 0, half);
+//     auto F = M2.submatrix(0, half, half);
+//     auto G = M2.submatrix(half, 0, half);
+//     auto H = M2.submatrix(half, half, half);
 
 
-    // compute intermediaries
-    Matrix<int> P1 = strassen_mult(A, matrix_sub(F, H));
-    Matrix<int> P2 = strassen_mult(matrix_add(A, B), H);
-    Matrix<int> P3 = strassen_mult(matrix_add(C, D), E);
-    Matrix<int> P4 = strassen_mult(D, matrix_sub(G, E));
-    Matrix<int> P5 = strassen_mult(matrix_add(A, D), matrix_add(E, H));
-    Matrix<int> P6 = strassen_mult(matrix_sub(B, D), matrix_add(G, H));
-    Matrix<int> P7 = strassen_mult(matrix_sub(C, A), matrix_add(E, F));
+//     // compute intermediaries
+//     Matrix<int> P1 = strassen_mult(A, matrix_sub(F, H));
+//     Matrix<int> P2 = strassen_mult(matrix_add(A, B), H);
+//     Matrix<int> P3 = strassen_mult(matrix_add(C, D), E);
+//     Matrix<int> P4 = strassen_mult(D, matrix_sub(G, E));
+//     Matrix<int> P5 = strassen_mult(matrix_add(A, D), matrix_add(E, H));
+//     Matrix<int> P6 = strassen_mult(matrix_sub(B, D), matrix_add(G, H));
+//     Matrix<int> P7 = strassen_mult(matrix_sub(C, A), matrix_add(E, F));
 
-    // combine results
-    // todo instead of making a new matrix here, just put it back in the original?
-    // should work, will wait till after I test this implementatino though
-    Matrix<int> M(n);
-    auto C11 = M.submatrix(0, 0, half);
-    auto C12 = M.submatrix(0, half, half);
-    auto C21 = M.submatrix(half, 0, half);
-    auto C22 = M.submatrix(half, half, half);
+//     // combine results
+//     // todo instead of making a new matrix here, just put it back in the original?
+//     // should work, will wait till after I test this implementatino though
+//     Matrix<int> M(n);
+//     auto C11 = M.submatrix(0, 0, half);
+//     auto C12 = M.submatrix(0, half, half);
+//     auto C21 = M.submatrix(half, 0, half);
+//     auto C22 = M.submatrix(half, half, half);
 
-    matrix_add_inplace(P5, P4, C11);
-    matrix_sub_inplace(C11, P2, C11);
-    matrix_add_inplace(C11, P6, C11);
-    matrix_add_inplace(P1, P2, C12);
-    matrix_add_inplace(P3, P4, C21);
-    matrix_add_inplace(P5, P7, C22);
-    matrix_sub_inplace(C22, P3, C22);
-    matrix_add_inplace(C22, P1, C22);
+//     matrix_add_inplace(P5, P4, C11);
+//     matrix_sub_inplace(C11, P2, C11);
+//     matrix_add_inplace(C11, P6, C11);
+//     matrix_add_inplace(P1, P2, C12);
+//     matrix_add_inplace(P3, P4, C21);
+//     matrix_add_inplace(P5, P7, C22);
+//     matrix_sub_inplace(C22, P3, C22);
+//     matrix_add_inplace(C22, P1, C22);
 
-    return M;
-};
+//     return M;
+// };
 
 size_t padding_calc(size_t dim) {
     if (dim <= CUTOFF) {
--- P2/triangle.hpp
+++ P2/triangle.hpp
@@ -9,7 +9,7 @@
 const float N10224_CHOOSE_3 = 178433024.;
 const size_t STANDARD_DIM    = 1024;
 const float CURR_PROB        = 0.1;
-const size_t NUM_TRIALS      = 1;
+const size_t NUM_TRIALS      = 5;
 
 // generates a random graph in adj list representation, where 
 // each edge has probability chance of existing
