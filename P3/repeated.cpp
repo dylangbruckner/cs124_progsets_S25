74d52ae9f8b2010f001f9e674ab0e30a21f45652
Tomas Gonzalez <tsgonzalez01@gmail.com>
2025-04-22 16:25:38 -0400

    completed repeated random function

---
+++ P3/repeated.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <random>
+
+std::vector<int> repeatedRandom(const std::vector<int>& input, const size_t& max_iter) {
+    size_t n = input.size();
+
+    std::vector<int> S = generateRandom(n);
+
+    for (size_t i = 0; i < max_iter; ++i) {
+        std::vector<int> temp = generateRandom(n);
+        
+        if (calculateResidue(input, temp, n) < calculateResidue(input, S, n)) {
+            S = temp;
+        }
+    }
+
+    return S;
+}
+
+std::vector<int> generateRandom(const size_t& n) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::bernoulli_distribution dist(0.5);
+
+    std::vector<int> output(n);
+
+    for (size_t i = 0; i < n; ++i) {
+        int randomValue = dist(gen) ? 1 : -1;
+        output[i] = randomValue;
+    }
+
+    return output;
+}
+
+int calculateResidue(const std::vector<int>& original, const std::vector<int>& S, const size_t& n) {
+    int residue = 0;
+
+    for (size_t i = 0; i < n; ++i) {
+        residue += original[i] * S[i];
+    }
+
+    return residue;
+}
