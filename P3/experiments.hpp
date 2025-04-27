17b35484fbdbf2f3911ebcc699a815b347b0a5f3
Tomas Gonzalez <tsgonzalez01@gmail.com>
2025-04-26 22:15:57 -0400

    edit prev commit

--- P3/experiments.hpp
+++ P3/experiments.hpp
@@ -2,8 +2,10 @@
 #define EXPERIMENTS_HPP
 #include "annealing.hpp"
 #include "hill.hpp"
+#include <chrono>
 
 std::vector<int64_t> problemInstance(const size_t& n);
 std::vector<uint64_t> experiments(const size_t n, const size_t num_trials, const size_t max_iter);
+std::vector<uint64_t> experiment_times(const size_t n, const size_t num_trials, const size_t max_iter);
 
 #endif // EXPERIMENTS_HPP
